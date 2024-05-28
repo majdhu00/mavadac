21211e71cf568b3695ab3f39856040b8f1a04d3b
madjhu00 <lil.m.11@hotmail.com>
2024-05-28 17:35:34 +0300

    aa

--- q3.c
+++ q3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    float base=5, height=3;
+    int base=5, height=3;
     double area;
 
     
@@ -15,8 +15,8 @@ int main() {
 
    
 
-  int base1=312.45 ,height1=20.012;
-    float area1;
+  float base1=312.45 ,height1=20.012;
+    double area1;
     printf("enter a triangle edge and its height respectively:%.2f %.3f \n",base1,height1 );
     area1 = 0.5 * base1 * height1;
      printf("the triangel aerea is: %.3f\n", area1);
